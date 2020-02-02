b3c316b87ca6a27fdce17cfea3ae1170f30ea50f
Arden97 <Artemden97@mail.ru>
2020-02-02 19:26:55 +0100

    add Click, comments, remove tools file

--- README.md
+++ README.md
@@ -9,13 +9,19 @@ fasdfasfadsfasf  sdfasdfsdfasfsd
 -> ls
 test1.docx  test2  test3.txt  test4  test5
 ```
+## Prerequisites
+For the script to work correctly, you will need a Click package. The easiest way to install it is:
+```
+pip install click
+```
+
 ### Installing
 Run these commands in your terminal:
 ```
 -> cd /home/user/bin
 -> git clone https://github.com/Arden97/renard.git
 -> cd renard
--> chmod +x renard.py renard_tools.py
+-> chmod +x renard.py
 ```
 Add this to your shell config file (.zshrc in my case):
 ```
@@ -24,6 +30,6 @@ export PATH=$PATH:/home/arden/bin/renard
 Now run:
 ```
 -> source ~/.zshrc
--> renard.py -h
+-> renard.py --help
 ```
-Help message will show up. Here you can see detailed information about the flags and options of the tool.
+Help message will show up. Here you can see detailed information about the options of the tool.
--- renard.py
+++ renard.py
@@ -1,17 +1,68 @@
-#!/usr/bin/python
+import os
+import sys
+import shutil
+import click
 
-import argparse
-from renard_tools import os, sort, rename
+# Sort files by size
+def size_sort(files, reverse):
+    file_size = {}
+    for file in files:
+        size = os.path.getsize(file)
+        file_size[file] = size
+        if (not reverse):
+            file_size = {k:v for k,v in sorted(file_size.items(), key=lambda item:item[1])}
+        else: file_size = {k:v for k,v in sorted(file_size.items(), key=lambda item:item[1], reverse=True)}
+    files = [file for file in file_size]
+    
+    return files
 
-# TODO: rename files in subdirectories
-parser = argparse.ArgumentParser(description = 'Rename files in the directory, using naming conventions')
-parser.add_argument('name', help = 'name convention')
-parser.add_argument('-d', '--directory', default=os.getcwd(), help = 'directory, which contains files')
-parser.add_argument('-n', '--number', type = int, help = 'number of files to rename')
-parser.add_argument('-i', '--include', action = 'store_true', default = False, help = 'rename subfolders as well')
-parser.add_argument('-s', '--sort', choices=['d', 'n', 's', 'rd', 'rn', 'rs'], default = 'n',
-                    help = 'sorting order [d - date, n - name, s - size, r[letter] - reverse order]')
+# Sort files based on arguments passed
+def sort(name, dir, num, inc, sort):
+    if(inc): 
+        files = [file for file in os.listdir(dir)]
+    else:
+        files = [file for file in os.listdir(dir) if os.path.isfile(dir+file)]
+    
+    if (sort):
+        if (sort == 'date'):
+            files.sort(key = os.path.getmtime)
+        elif (sort == 'name'):
+            files.sort()
+        elif (sort == 'size'):
+            files = size_sort(files, False)
+        elif (sort == 'rdate'):
+            files.sort(key = os.path.getmtime, reverse = True)
+        elif (sort == 'rname'):
+            files.sort(reverse = True)
+        elif (sort == 'rsize'):
+            files = size_sort(files, True) 
+    if (num):
+        del files[len(files)-num:]
 
-args = parser.parse_args()
-files = sort(args)
-rename(args, files)
+    return files
+
+def rename(files, dir, name):
+    for idx,file in list(enumerate(files, start = 1)):
+        ext = os.path.splitext(file)[1]
+        os.rename(dir+file, dir+name+str(idx)+ext)
+
+# Arguments and options
+# TODO: add feature to sort and rename files in subfolders as well
+@click.command()
+@click.argument('name')
+@click.option('--dir', default=os.getcwd(), help = 'Directory, which contains files')
+@click.option('--num', type = int, help = 'Number of files to rename  [default: all]')
+@click.option('--inc', is_flag=True, help = 'Include subfolders in renaming process')
+@click.option('--sort', type=click.Choice(['date', 'name', 'size', 'rdate', 'rname', 'rsize']),
+                default = 'name', help = 'Sorting order', show_default=True)
+def main(name, dir, num, inc, sort):
+    """
+    Rename files in the directory, using naming conventions.
+    """
+    files = sort(name, dir, num, inc, sort)
+    rename(files, dir, name)
+
+
+
+if __name__ == "__main__":
+    main() # pylint: disable=no-value-for-parameter
